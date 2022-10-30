e09083ad8f12ff8b79f7497704d1412319739034
max <maximiliangibes@gmail.com>
2022-10-30 06:30:55 -0600

    added when the user confirms they want lowercase,uppercase,numbers,special character, it grabs from the index of the array randomly, not a number the length of the array randomly

--- Develop/script.js
+++ Develop/script.js
@@ -16,7 +16,7 @@ var upperArr = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
 
 var numbersArr = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-var specialArr = ['!', '@', '#', '$', '*', '%'];
+var specialArr = ['!', '@', '#', '$', '*', '%', '&'];
 
 // Add event listener to generate button
 
@@ -33,22 +33,26 @@ if(!passLength) {
 }
 generatepass();
 
+//allows a path to the lowerArr index 
 var passLowerCase = confirm("Do you want your password to include lowercase values?");
-
 var lowerIndex = Math.floor(Math.random() * lowerArr.length);
-console.log(lowerIndex);
+var userLower = lowerArr[lowerIndex];
+console.log(userLower);
 
+//allows a path to the upperArr index
 var passUpperCase = confirm("Do you want your password to include uppercase values?");
-
 var upperIndex = Math.floor(Math.random() * upperArr.length);
-console.log(upperIndex);
+var userUpper = upperArr[upperIndex];
+console.log(userUpper);
 
+//allows a path to the numbersArr index
 var passNumberic = confirm("Do you want your password to include numeric value?");
+var numbersIndex = Math.floor(Math.random() * numbersArr.length);
+var userNumber = numbersArr[numbersIndex];
+console.log(userNumber);
 
-var numberIndex = Math.floor(Math.random() * numbersArr.length);
-console.log(numberIndex);
-
+//allows a path to the specialArr index
 var passSpecial = confirm("Do you want your password to include special character values?");
-
 var specialIndex = Math.floor(Math.random() * specialArr.length);
-console.log(specialIndex);
+var userSpecial = specialArr[specialIndex];
+console.log(userSpecial);
